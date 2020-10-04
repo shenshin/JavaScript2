32660d227e70e17b70ec9cb85cb67b0590dd04d9
Ales Shenshin <shenshin@me.com>
2020-10-04 02:22:30 +0200

    Add class examples instead of closures approach to exercise 1

--- Week3/js-exercises/ex1-AddSix.js
+++ Week3/js-exercises/ex1-AddSix.js
@@ -9,19 +9,48 @@ Call the function three times. The return values should be:
  15, 24, 33
 
  */
-
-function createBase(number) {
+// It looks weird to me and blows my mind:
+// function that returnes a function that returnes a function!
+function createBase(numberX) {
   // Put here your logic...
-  let capturedVar = number
-  return function () {
-    capturedVar += 9
-    return capturedVar
+  let property = numberX
+  return function (numberY) {
+    property += numberY
+    return property
   }
 }
 
 const addSix = createBase(6);
 
 // Put here your function calls...
-console.log(addSix())
-console.log(addSix())
-console.log(addSix())
+console.log(addSix(9)) // 15
+console.log(addSix(9)) // 24
+console.log(addSix(9)) // 33
+
+// Next approach does the same but looks absolutly sane to me!!!
+function Base1(numberX) {
+  this.property = numberX
+}
+Base1.prototype.add = function (NumberY) {
+  this.property += NumberY
+  return this.property
+}
+const base1 = new Base1(6)
+console.log(base1.add(9)) // 15
+console.log(base1.add(9)) // 24
+console.log(base1.add(9)) // 33
+
+// And next one - even more sane
+class Base2 {
+  constructor(numberX) {
+    this.capturedVar = numberX
+  }
+  add(numberY) {
+    this.capturedVar += numberY
+    return this.capturedVar
+  }
+}
+const base2 = new Base2(6)
+console.log(base2.add(9)) // 15
+console.log(base2.add(9)) // 24
+console.log(base2.add(9)) // 33
