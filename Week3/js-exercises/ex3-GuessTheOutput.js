7ef1b59c4a8be59c794a7303045418857ed04d86
Ales Shenshin <shenshin@me.com>
2020-10-04 02:32:54 +0200

    I add some clarifications to the commentary to the exercise 3

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -9,11 +9,12 @@ Write out your reasoning in 50 words or less.
 */
 
 /* Variable 'a' is defined in the global scope, so it is accessible
-in all functions defined later. Code inside function x 
+in all functions defined in the program. Code inside function x 
 definition changes variable 'a' and assigns new value 12 to it.
 Then the closure returning by x captures 'a' from it's parent function scope
 and passes it to alert function. So the alert is about to show string 
-represtation of 'a' which is '12' */
+represtation of 'a' which is '12'. Next x is called at the time of
+it's definition with pair of brackets () and shows the alert */
 let a = 10;
 const x = (function () {
   a = 12;
