9016fa2346709d4399cf367e26050657944ffbba
Ales Shenshin <shenshin@me.com>
2020-10-09 15:32:59 +0200

    Fix generating random array index to get random quote in Project

--- Week1/project/index.js
+++ Week1/project/index.js
@@ -4,19 +4,21 @@
 const quoteNode = document.querySelector('.quote span')
 const authorNode = document.querySelector('.author span')
 const newQuoteButton = document.querySelector('.button:last-child')
-
+let data
 // send request to remote API
 fetch("https://type.fit/api/quotes")
   .then(response => response.json())
-  .then(data => {
-    // get new quote right when after getting a responce
+  .then(newData => {
+    data = newData
+    // get new quote right when after getting a response
     if (!quoteNode.innerHTML) getNewQuote()
     // add click listener to 'New quote' button
     newQuoteButton.addEventListener('click', getNewQuote)
     // on button click get a random quote and paste to HTML
-    function getNewQuote(event) {
-      const randomIndex = Math.floor(Math.random() * (data.length - 1))
-      quoteNode.innerHTML = data[randomIndex].text
-      authorNode.innerHTML = data[randomIndex].author ?? 'Unknown Author'
-    }
+
   })
+function getNewQuote(event) {
+  const randomIndex = Math.floor(Math.random() * data.length)
+  quoteNode.innerHTML = data[randomIndex].text
+  authorNode.innerHTML = data[randomIndex].author ?? 'Unknown Author'
+}
