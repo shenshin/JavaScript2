68df0c24a7d389c5ebf312264d0f543d744f4415
Ales Shenshin <shenshin@me.com>
2020-10-10 16:35:23 +0200

    Small fix in quote generator

--- Week1/project/index.js
+++ Week1/project/index.js
@@ -1,21 +1,26 @@
 'use strict';
 
+let quotesArray;
 // create references to HTML elements
 const quoteNode = document.querySelector('.quote span');
 const authorNode = document.querySelector('.author span');
-const newQuoteButton = document.querySelector('.button:last-child');
-let quotesArray;
+// add click listener to 'New quote' button
+document.querySelector('.button:last-child').addEventListener('click', () => {
+  if (quotesArray) {
+    console.log(quotesArray.length);
+    getNewQuote();
+  }
+});
+
 // send request to remote API
 fetch('https://type.fit/api/quotes')
-  .then((response) => response.json())
-  .then((newData) => {
+  .then(response => response.json())
+  .then(newData => {
     quotesArray = newData;
     // get new quote right after getting a response
     if (!quoteNode.innerHTML) getNewQuote();
-    // add click listener to 'New quote' button
-    newQuoteButton.addEventListener('click', getNewQuote);
-    // on button click get a random quote and paste to HTML
   });
+// on button click get a random quote and paste to HTML
 function getNewQuote() {
   const randomIndex = Math.floor(Math.random() * quotesArray.length);
   quoteNode.innerHTML = quotesArray[randomIndex].text;
